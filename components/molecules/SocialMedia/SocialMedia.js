1ba7ea8bdd2614ee4fa6d937ca7ba3946a7d4a5e
WiktorSmura <wiktorsmura24@gmail.com>
2022-08-17 19:01:54 +0200

    socialmedia changes

--- components/molecules/SocialMedia/SocialMedia.js
+++ components/molecules/SocialMedia/SocialMedia.js
@@ -1,11 +1,18 @@
 import styles from "./SocialMedia.module.scss";
 import InstagramIcon from "/public/svg/instagram-svgrepo-com.svg";
+import { FaFacebookF } from "react-icons/fa";
 
 function SocialMedia() {
   return (
     <section className={styles.socialMedia}>
-      <InstagramIcon className={styles.icon} />
-      <h2 className={styles.heading}>Sprawdź nasz instagram</h2>
+      <div className={styles.wrapper}>
+        <InstagramIcon className={styles.icon} />
+        <h2 className={styles.heading}>Instagram</h2>
+      </div>
+      <div className={styles.wrapper}>
+        <FaFacebookF className={styles.icon} />
+        <h2 className={styles.heading}>Facebook</h2>
+      </div>
     </section>
   );
 }
