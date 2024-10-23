941b532900c95d8fb87f743fc3f944fd2377a2ea
WiktorSmura <wiktorsmura24@gmail.com>
2024-10-23 22:06:32 +0200

    new menu chanhes

--- components/organisms/Hero/Hero.js
+++ components/organisms/Hero/Hero.js
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
 import styles from "./Hero.module.scss";
 import Image from "next/image";
 import Divider from "/public/svg/wave-divider.svg";
@@ -5,6 +7,16 @@ import Logo from "/public/svg/logo.svg";
 import { AiOutlineArrowDown } from "react-icons/ai";
 
 function Hero() {
+  const advertRef = useRef();
+
+  useEffect(() => {
+    gsap.fromTo(
+      advertRef.current,
+      { x: -1000, opacity: 0 },
+      { x: 0, opacity: 1, duration: 2, delay: 0.5 }
+    );
+  });
+
   return (
     <>
       <section className={styles.hero}>
@@ -18,6 +30,10 @@ function Hero() {
           priority
         />
         <Logo className={styles.logo} />
+        <div className={styles.advert} ref={advertRef}>
+          <h3>Zamów 4 pizze --&gt; zapłać za 3</h3>
+          <h3>(czwartą najtańszą otrzymasz GRATIS!)</h3>
+        </div>
         <button className={styles.callToAction} aria-label="arrow down">
           <AiOutlineArrowDown className={styles.arrow} />
         </button>
